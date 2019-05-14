f7adba0d2385e75c5f3b41ba9e9d264980b8974b
yvlian <2509123408@qq.com>
2019-05-14 20:31:54 +0800

    get my feature data

---
+++ this/get_my_feature_data.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+d1 = pd.read_csv('../data/security_train.csv')
+d1['file_id'] = ['train'+str(fid) for fid in d1['file_id']]
+y = d1[['file_id','label']].groupby('file_id').mean()
+d1.pop('label')
+
+d2 = pd.read_csv('../data/security_test.csv')
+d2['file_id'] = ['test'+str(fid) for fid in d2['file_id']]
+
+d = pd.concat([d1,d2],axis=0)
+d.reset_index(inplace=True)
+
+api_dictionary = list(set(d['api']))
+
+tids_per_file = dict()
+data = pd.DataFrame(columns=api_dictionary)
+file_ids = list(set(d['file_id']))
+file_ids.sort(key = list(d['file_id']).index)
+for fid in file_ids:
+    mask1 = (d['file_id'] == fid)
+    tid_mask1 = d['tid'][mask1]
+    tids = list(set(tid_mask1))
+    new_row = {'file_id':fid,'n_threads':len(tids)}
+    api_lengths = []
+    for i in range(len(tids)):
+        tid = tids[i]
+        mask2 = (d['tid'] == tid)
+        api_lengths.append(len(tid_mask1[mask2]))
+        apis = d['api'][mask1][mask2]
+        for api in apis:
+            if api not in new_row.keys():
+                new_row[api] = 1
+                continue
+            new_row[api] += 1
+    api_lengths.sort(reverse=True)
+    for i in range(len(api_lengths)):
+        new_row['api_length' + str(i)] = api_lengths[i]
+    new_row['total_api_length'] = sum(api_lengths)
+    data = data.append(new_row, ignore_index=True)
+data = data.fillna(0)
+data1 = data[:len(y)]
+data1['label'] = list(y['label'])
+data1.to_csv('../data/train.csv')
+data2 = data[len(y):]
+
+data2.to_csv('../data/test.csv')
+
--- this/t.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-d = pd.read_csv('../data/security_train.csv')
-print(1)
-a=1
-
