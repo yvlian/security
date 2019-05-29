170c49f7f738e31003c99bcb36a7ba6586a8779a
yvlian <2509123408@qq.com>
2019-05-29 08:55:04 +0800

    add print info

--- this/get_my_feature_data.py
+++ this/get_my_feature_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+print('---')
 d1 = pd.read_csv('../data/security_train.csv')
 d1['file_id'] = ['train'+str(fid) for fid in d1['file_id']]
 y = d1[['file_id','label']].groupby('file_id').mean()
@@ -17,6 +17,8 @@ tids_per_file = dict()
 data = pd.DataFrame(columns=api_dictionary)
 file_ids = list(set(d['file_id']))
 file_ids.sort(key = list(d['file_id']).index)
+
+print('get my data feature,including file_id,APIs_count,api_length,total_api_length,n_threads,label')
 for fid in file_ids:
     mask1 = (d['file_id'] == fid)
     tid_mask1 = d['tid'][mask1]
@@ -38,6 +40,8 @@ for fid in file_ids:
         new_row['api_length' + str(i)] = api_lengths[i]
     new_row['total_api_length'] = sum(api_lengths)
     data = data.append(new_row, ignore_index=True)
+
+print('train.csv')
 data = data.fillna(0)
 data1 = data[:len(y)]
 data1['label'] = list(y['label'])
@@ -45,6 +49,7 @@ data1['file_id'] = data1['file_id'].apply(lambda x:x.replace('train','')).tolist
 data1 = data1.set_index('file_id')
 data1.to_csv('../data/train.csv')
 
+print('test.csv')
 data2 = data[len(y):]
 data2['file_id'] = data2['file_id'].apply(lambda x:x.replace('test','')).tolist()
 data2 = data2.set_index('file_id')
